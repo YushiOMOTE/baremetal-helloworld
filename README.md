b3b993ea669072eab6601fd827f63fb1167be2a7
Yushi OMOTE <yushiomote@gmail.com>
2019-12-23 22:12:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# A x86 bare-metal hello-world in Rust
+
+```
+$ git submodule update --init
+$ ./build.sh
+```
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga = 0xb8000 as *mut u8;
+
+    for (i, b) in b"Hello world".iter().enumerate() {
+        unsafe {
+            *vga.offset(i as isize * 2) = *b;
+            *vga.offset(i as isize * 2 + 1) = 0x0f;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(__info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
